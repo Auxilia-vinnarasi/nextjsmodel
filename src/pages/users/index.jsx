32b3a5987b9e45c329e479ad881c71764cccc8af
Auxilia <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2023-02-21 17:02:38 +0530

    first commit

---
+++ src/pages/users/index.jsx
@@ -0,0 +1,14 @@
+import styles from "../../styles/Home.module.css";
+import React from "react";
+
+function userList(){
+    return (
+        <div className={styles.main}>
+            <h1>user 1</h1>
+            <h1>user 2</h1>
+            <h1>user 3</h1>
+        </div>
+    )
+}
+
+export default userList
